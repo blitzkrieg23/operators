d9b76954d5e8330ca9d292cf17288f08c096b1f4
xzqt23 <angelo.agualada@gmail.com>
2023-02-16 11:46:59 +0800

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,22 @@
+print(2 + 3) # Addition
+print (5 - 1)# Subtraction 
+print(8 * 8) # Multiplication
+print(10 / 2) # Division
+print(4 ** 3) # Exponentiation (same as 4 * 4 * 4)
+print( 15 // 2) # Floor Division (rounds the result down to the nearest whole number)
+
+
+
+"""
+==	Equal	       x == y	
+!=	Not equal	   x != y	
+>	Greater than   x > y	
+<	Less than	   x < y	
+>=	Greater than or equal to	x >= y	
+<=	Less than or equal to       x <= y
+
+"""
+
+print(2 > 1 and 1 == 1)
+print(2 > 1 or 5 > 6)
+print(not(5 > 4)) # return false if the result is true and vice versa
